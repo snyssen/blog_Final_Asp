5b6eb6b74e3e3eadb3229457a47f42d3c3141f1c
Simon Nyssen <simon.nyssen@outlook.com>
2018-12-27 11:34:47 +0100

    + Espace de gestion des comptes + Corrections mineures sur les layout

---
+++ Blog_final_Asp/Areas/Account/AccountAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Blog_final_Asp.Areas.Account
+{
+    public class AccountAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Account";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Account_default",
+                "Account/{controller}/{action}/{id}",
+                new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
