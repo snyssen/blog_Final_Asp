e60ffa12aba598b8e9284fc804ae8d8a06ea1565
Simon Nyssen <simon.nyssen@outlook.com>
2018-12-28 15:21:44 +0100

    + formulaire de post d'un nouveau billet prêt -> [WIP] gestion de la requête POST associée

---
+++ Blog_final_Asp/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog_final_Asp.ViewModels;
+
+namespace Blog_final_Asp.Controllers
+{
+    public class PostController : Controller
+    {
+        // GET: Post
+        [Authorize(Roles = ("Admin,Moderateur,Auteur"))]
+        public ActionResult Index() // Gère l'ajout d'un billet
+        {
+            PostIndexViewModel vm = new PostIndexViewModel { };
+            return View(vm);
+        }
+
+        [Authorize(Roles = ("Admin,Moderateur,Auteur"))]
+        [HttpPost]
+        public ActionResult Index(PostIndexViewModel vm)
+        {
+            return View(vm);
+        }
+
+        public ActionResult ID(int ID) // Affiche un billet par son ID
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Blog_final_Asp/ViewModels/PostIndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog_final_Asp.ViewModels
+{
+    public class PostIndexViewModel
+    {
+        [Required(ErrorMessage = "Veuillez indiquer le titre du billet")]
+        [Display(Name = "Titre")]
+        [MaxLength(50, ErrorMessage = "Ce titre est trop long")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Veuillez rentrer le contenu du billet")]
+        [Display(Name = "Corps")]
+        public string Body { get; set; }
+        [Display(Name = "Photo de couverture (optionnelle)")]
+        [FileExtensions(Extensions = "jpg,png,gif", ErrorMessage = "Veuillez entrer une image au format .jpg, .png ou .gif")]
+        public HttpPostedFileBase Picture { get; set; }
+    }
+}
